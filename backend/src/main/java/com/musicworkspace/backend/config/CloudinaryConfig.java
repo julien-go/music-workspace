c214b331575f00da5fb0653ee54e3db6002dc48f
julien-go <juliengodard97@gmail.com>
2026-07-03 15:42:14 +0200

    fix ci cloudinary

--- backend/src/main/java/com/musicworkspace/backend/config/CloudinaryConfig.java
+++ backend/src/main/java/com/musicworkspace/backend/config/CloudinaryConfig.java
@@ -2,10 +2,16 @@ package com.musicworkspace.backend.config;
 
 import com.cloudinary.Cloudinary;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ConditionalOnProperty(
+    name = "cloudinary.enabled",
+    havingValue = "true",
+    matchIfMissing = true
+)
 public class CloudinaryConfig {
 
     @Value("${cloudinary.url}")
