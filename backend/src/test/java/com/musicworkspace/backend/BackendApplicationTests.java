c560a446bdef98d0e15c896f3ce8649e3f4a2f5b
julien-go <juliengodard97@gmail.com>
2026-07-03 15:31:36 +0200

    fix postgresTestContainer

--- backend/src/test/java/com/musicworkspace/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/musicworkspace/backend/BackendApplicationTests.java
@@ -2,7 +2,9 @@ package com.musicworkspace.backend;
 import com.musicworkspace.backend.config.PostgresTestContainer;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 @SpringBootTest
+@ActiveProfiles("test")
 class BackendApplicationTests extends PostgresTestContainer {
 
 	@Test
