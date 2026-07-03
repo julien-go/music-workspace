cf8f3e295ddfb216f8b69c1e5b78596b4471bac0
julien-go <juliengodard97@gmail.com>
2026-07-03 14:57:30 +0200

    fix ci backend

--- backend/src/test/java/com/musicworkspace/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/musicworkspace/backend/BackendApplicationTests.java
@@ -2,7 +2,8 @@ package com.musicworkspace.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+import org.springframework.test.context.ActiveProfiles;
+@ActiveProfiles("test")
 @SpringBootTest
 class BackendApplicationTests {
 
