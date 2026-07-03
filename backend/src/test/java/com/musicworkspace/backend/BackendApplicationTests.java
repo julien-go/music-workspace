4970242aa636e17efae6b0cdc9282dd1143c5bbc
julien-go <juliengodard97@gmail.com>
2026-07-03 15:55:18 +0200

    fix ci backend

--- backend/src/test/java/com/musicworkspace/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/musicworkspace/backend/BackendApplicationTests.java
@@ -3,8 +3,10 @@ import com.musicworkspace.backend.config.PostgresTestContainer;
 import com.musicworkspace.backend.config.CloudinaryTestMockConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
+@ActiveProfiles("test")
 @Import(CloudinaryTestMockConfig.class)
 class BackendApplicationTests extends PostgresTestContainer {
 
--- backend/src/test/java/com/musicworkspace/backend/config/CloudinaryTestMockConfig.java
+++ backend/src/test/java/com/musicworkspace/backend/config/CloudinaryTestMockConfig.java
@@ -4,13 +4,13 @@ import com.cloudinary.Cloudinary;
 import org.mockito.Mockito;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 
 @TestConfiguration
+@Profile("test")
 public class CloudinaryTestMockConfig {
 
     @Bean
-    @Primary
     public Cloudinary cloudinary() {
         return Mockito.mock(Cloudinary.class);
     }
